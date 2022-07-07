7d29f3d1d9f2ab04f35a7f4b5624b9525106eee9
Kortney <kortney@iMac.local>
2022-07-07 13:47:01 -0300

    added filter in the views that will check if there is a query parameter that matches the one we give it and filters results to that param

--- pokemonCards/pokemoncard_api/views.py
+++ pokemonCards/pokemoncard_api/views.py
@@ -5,4 +5,10 @@ from rest_framework import viewsets
 
 class PokemonCardViewset(viewsets.ModelViewSet):
     serializer_class = PokemonCardSerializer
-    queryset = PokemonCard.objects.all()
+
+    def get_queryset(self):
+        queryset = PokemonCard.objects.all()
+        pokemonType = self.request.query_params.get('pokemontype')
+        if pokemonType is not None:
+            queryset = queryset.filter(pokemonType=pokemonType)
+        return queryset
